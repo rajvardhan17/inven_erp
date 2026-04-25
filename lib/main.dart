52f2a030c5bf51872e88fb1fe14a750c1eec8976
unknown <rajvardhansingh1703@gmail.com>
2026-04-25 14:26:37 +0530

    stablizing the project

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// 👇 IMPORT THIS
+import 'firebase_options.dart';
+import 'features/auth/presentation/login_screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
 
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // ✅ FIX
   );
+
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,11 +21,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Scaffold(
-        body: Center(child: Text("Intellectium ERP 🚀")),
-      ),
+      home: LoginScreen(),
     );
   }
 }
